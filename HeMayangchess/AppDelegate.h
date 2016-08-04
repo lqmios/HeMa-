272f7949082970c3bcba4257d17c32d71f7d52a9
lqm <lqm@lqmdemacbook-pro.local>
2016-08-04 10:35:49 +0800

    Initial Commit

---
+++ HeMayangchess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeMayangchess
+//
+//  Created by lqm on 16/8/4.
+//  Copyright © 2016年 LQM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
